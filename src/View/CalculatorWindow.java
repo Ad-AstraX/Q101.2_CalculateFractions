f6a23ea239bf5fa8d7dccfc2392ec36e8ef1bad1
moushaya <hayamou2006@yahoo.com>
2023-08-25 11:18:26 +0200

    feat(src): added form

---
+++ src/View/CalculatorWindow.java
@@ -0,0 +1,19 @@
+package View;
+
+import javax.swing.*;
+
+public class CalculatorWindow extends JFrame {
+    private JComboBox chooseOperator;
+    private JButton confirmButton;
+    private JFormattedTextField secondDenom;
+    private JFormattedTextField secondNom;
+    private JFormattedTextField resultDenom;
+    private JFormattedTextField resultNom;
+    private JFormattedTextField firstNom;
+    private JFormattedTextField firstDenom;
+    private JLabel operator;
+
+    public CalculatorWindow() {
+
+    }
+}
